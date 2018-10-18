c6afefcc04e3c27a33a817c5bf10cfd5ab848370
Andrew Brice <abrice@sharpencx.com>
2018-10-18 13:05:28 -0400

    better color nomenclature

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,9 @@ class App extends Component {
       latitude: "",
       longitude: "",
       weather: "",
-      weatherColor1: "",
-      weatherColor2: "",
+      lowTempColor: "",
+      highTempColor: "",
+      feelsLikeColor: "",
       userConfigured: false,
       showDashboard: false,
       currentQuote: {
@@ -42,42 +43,54 @@ class App extends Component {
       .then(res => res.json())
       .then(weather => {
         this.setState({ weather: weather });
-        this.calculateWeatherColors(this.state.weather.daily.data[0]);
+        this.calculateWeatherColors(this.state.weather);
       });
   }
 
   //determine an HSL color valyue based on the temperature and cloud cover
   calculateWeatherColors = (data) => {
 
+    let today = data.daily.data[0];
+    let currently = data.currently;
+
     //we'll use the temperature value to determine our hues
 
+    //grab the day's apprent low temp
+    let lowTemp = Math.floor(today.apparentTemperatureLow);
+    //highest temp on our scale is 100 (anything over will be calculated as 100)
+    lowTemp = lowTemp > 100 ? 100 : lowTemp;
+    //lowest temp on our scale is 0 (anything under will be calculated as 0)
+    lowTemp = lowTemp < 0 ? 0 : lowTemp;
+
     //grab the day's apprent high temp
-    let highTemp = Math.round(data.apparentTemperatureHigh);
+    let highTemp = Math.round(today.apparentTemperatureHigh);
     //highest temp on our scale is 100 (anything over will be calculated as 100)
     highTemp = highTemp > 100 ? 100 : highTemp;
     //lowest temp on our scale is 0 (anything under will be calculated as 0)
     highTemp = highTemp < 0 ? 0 : highTemp;
 
     //grab the day's apprent low temp
-    let lowTemp = Math.floor(data.apparentTemperatureLow);
+    let feelsLikeTemp = Math.floor(currently.apparentTemperature);
     //highest temp on our scale is 100 (anything over will be calculated as 100)
-    lowTemp = lowTemp > 100 ? 100 : lowTemp;
+    feelsLikeTemp = feelsLikeTemp > 100 ? 100 : feelsLikeTemp;
     //lowest temp on our scale is 0 (anything under will be calculated as 0)
-    lowTemp = lowTemp < 0 ? 0 : lowTemp;
+    feelsLikeTemp = feelsLikeTemp < 0 ? 0 : feelsLikeTemp;
 
     //calculate our hues...
     //coldest value is 240, warmest is 0 (on our hue scale)
-    let hue1 = Math.round(240 - (240 * (highTemp / 100)));
-    let hue2 = Math.round(240 - (240 * (lowTemp / 100)));
+    let lowHue = Math.round(240 - (240 * (lowTemp / 100)));
+    let highHue = Math.round(240 - (240 * (highTemp / 100)));
+    let feelsLikeHue = Math.round(240 - (240 * (feelsLikeTemp / 100)));
 
     //we'll use the cloudCover value to determine our saturation
-    let cloudCover = data.cloudCover;
+    let cloudCover = today.cloudCover;
     //our mininum saturation will be 50 (max is 100%)
     let saturation = 50 + (50 - (50 * cloudCover));
 
     //these two colors will be used to render our weather color gradient
-    this.setState({ weatherColor1: `hsl(${hue1}, ${saturation}%, 50%)` });
-    this.setState({ weatherColor2: `hsl(${hue2}, ${saturation}%, 50%)` });
+    this.setState({ lowTempColor: `hsl(${lowHue}, ${saturation}%, 50%)` });
+    this.setState({ highTempColor: `hsl(${highHue}, ${saturation}%, 50%)` });
+    this.setState({ feelsLikeColor: `hsl(${feelsLikeHue}, ${saturation}%, 50%)` });
   }
 
   //Retrieve a quote from the Internet Chuck Norris Database
@@ -167,14 +180,14 @@ class App extends Component {
 
         <section
           className="dashboard"
-          style={{ background: `linear-gradient(180deg, ${this.state.weatherColor1}, ${this.state.weatherColor2})`}}>
+          style={{ background: `linear-gradient(180deg, ${this.state.highTempColor}, ${this.state.lowTempColor})`}}>
 
           {this.state.weather.daily &&
             <Temperature weather={this.state.weather} tempType="high" />
           }
 
           {this.state.weather !== null &&
-            <Weather locationLabel={this.state.locationLabel} weather={this.state.weather} />
+            <Weather locationLabel={this.state.locationLabel} weather={this.state.weather} feelsLikeColor={this.state.feelsLikeColor} />
           }
 
           <Quote quote={this.state.currentQuote} onClick={this.fetchQuote} />
